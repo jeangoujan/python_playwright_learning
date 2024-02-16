c717b074e5b06f8eacc929bf9cc793b6d3a2455f
DamirZH <damir.zhanguzhanov@list.ru>
2024-02-16 21:29:37 +0600

    Добавил тестов

--- pythonProject1/pages/home_page.py
+++ pythonProject1/pages/home_page.py
@@ -3,12 +3,27 @@ import config
 
 
 class HomePage:
+    # Header
     _BUTTON_HALYK_LOGIN = f'text=Войти'
     _BUTTON_SELECT_CITY = '[aria-label="Смена города, текущий город"]'
     _BUTTON_SELECT_LANGUAGE = '//div[@class="panel-item panel-item-dropdown" and contains(text(), "Русский")]'
     _BUTTON_HALYK_BANK = '//div[@class="panel-item" and contains(text(), "Halyk bank")]'
     _BUTTON_HOMEBANK = '//div[@class="panel-item" and contains(text(), "Homebank")]'
     _BUTTON_CALL_CENTER = '//div[@class="panel-item" and contains(text(), "77 111")]'
+
+    # Main Header
+    _HALYK_MARKET_LOGO = '//img[@src="/_nuxt/30f70ea43b8383d02d9fade5c7f222d7.svg"]'
+    _HALYK_SEARCH_BAR = 'input.search-input[placeholder="Найти в Halyk Market"]'
+    _HALYK_FAVORITES = '//span[contains(@class, "icon-text") and contains(text(), "Избранное")]'
+    _HALYK_CART = '//span[contains(@class, "icon-text") and contains(text(), "Корзина")]'
+
+    # Category-bar --------------------------------------------------------------------------------
+    _CATEGORY_BUTTON = 'button.btn.navbar-button.navbar-trigger.filled.small[aria-label="Просмотреть все категории"]'
+    _PROMOTIONS_BUTTON = 'li.navbar-item > a[href="/promotion/all"]'
+    _PHONES_GADGETS = 'li.navbar-item > a[href="/category/telefoni-i-gadzheti"]'
+    _KITCHEN_EQUIPMENT_BUTTON = 'li.navbar-item > a[href="/category/kuhonnaya-tehnika"]'
+    _SPORT_BUTTON = 'li.navbar-item > a[href="/category/dosug"]'
+    _FURNITURE_BUTTON = 'li.navbar-item > a[href="/category/mebel"]'
 
     def __init__(self, page: Page):
         self.page = page
@@ -18,17 +33,3 @@ class HomePage:
 
     def is_element_visible(self, selector):
         return self.page.locator(selector).is_visible()
-    # def get_text_from_elements(self, page: Page, text) -> None:
-    #     page.get_by_text(self, text)
-    #
-    # def get_text_from_halyk_login_button(self, page: Page) -> None:
-    #     return page.locator(self._BUTTON_HALYK_LOGIN).get_attribute('value')
-    #
-    # def logo_is_available(self, page: Page):
-    #     return page.get_by_alt_text(self._HALYK_LOGO).nth(0).is_visible()
-    #
-    # def get_text_from_halyk_search_bar(self, page: Page):
-    #     return page.locator(self._HALYK_SEARCH_BAR).nth(0).is_visible()
-
-# def select_city_from_cities_list(self, page: Page): -> None:
-#     return
--- pythonProject1/tests/test_home_page.py
+++ pythonProject1/tests/test_home_page.py
@@ -1,28 +1,83 @@
 import pytest
-import pages
 from pages.home_page import HomePage
 
 
 class TestHomePage:
+    # Header: City, Language, Halyk Bank, Homebank, 77-111, Login ---------------------------------------
     @pytest.fixture()
     def home_page(self, page):
         return HomePage(page)
 
     def test_login_button_is_available(self, home_page, page):
         home_page.open_page()
-        assert home_page.is_element_visible(home_page._BUTTON_HALYK_LOGIN), "Кнопка 'Войти' не отображается на странице."
-    # def test_home_page_is_available(self, page):
-    #     pass
-    #     # pages.home_page.open_page(page)
-    #     # actual_result = pages.home_page.logo_is_available(page)
-    #
-    # def test_search_is_available(self, page):
-    #     pass
-    #     # pages.home_page.open_page(page)
-    #     # assert pages.home_page.get_text_from_halyk_search_bar(page) == True, "Поиск на главной странице недоступен"
-    #
+        assert home_page.is_element_visible(home_page._BUTTON_HALYK_LOGIN), "Кнопка 'Войти' недоступна"
 
-    # def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
-    #     pages.index_page.open_index_page(page)
-    #     actual_result = pages.index_page.get_text_from_halyk_login_button(page)
-    #     assert actual_result == ' Войти ', 'Не найдено'
+    def test_city_selector_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._BUTTON_SELECT_CITY), "Кнопка выбора города недоступна"
+
+    def test_language_selector_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._BUTTON_SELECT_LANGUAGE), "Кнопка выбора языка недоступна"
+
+    def test_halyk_bank_button_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._BUTTON_HALYK_BANK), "Кнопка 'Halyk Bank' недоступна"
+
+    def test_homebank_button_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._BUTTON_HOMEBANK), "Кнопка 'Homebank' недоступна"
+
+    def test_call_center_button_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._BUTTON_CALL_CENTER), "Кнопка '77-111' недоступна"
+
+    # Header: Logo, Search-bar, Favorites, Cart ---------------------------------------------------------
+    def test_logo_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._HALYK_MARKET_LOGO), "Логотип недоступен"
+
+
+    def test_search_bar_is_available(self, home_page, page):
+        home_page.open_page()
+        search_bar = home_page.page.locator(home_page._HALYK_SEARCH_BAR)
+        assert search_bar.first.is_visible(), "Поиск недоступен"
+
+    def test_favorites_is_available(self, home_page, page):
+        home_page.open_page()
+        favorites = home_page.page.locator(home_page._HALYK_FAVORITES)
+        assert favorites.first.is_visible(), "Избранные товары недоступны"
+
+    def test_cart_is_available(self, home_page, page):
+        home_page.open_page()
+        cart = home_page.page.locator(home_page._HALYK_CART)
+        assert cart.first.is_visible(), "Корзина недоступна"
+
+    # Category-bar: Category, Promotions, Phones and Gadgets, Kitchen Equip, Sport, Furniture
+    @pytest.mark.first
+    def test_category_button_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._CATEGORY_BUTTON), "Кнопка Категории недоступна"
+
+    @pytest.mark.first
+    def test_promotions_button_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._PROMOTIONS_BUTTON), "Кнопка 'Акции Halyk Market' недоступна"
+
+    @pytest.mark.first
+    def test_phones_button_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._PHONES_GADGETS), "Кнопка 'Телефоны и гаджеты' недоступна"
+
+    def test_kitchen_equip_button_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._KITCHEN_EQUIPMENT_BUTTON), "Кнопка 'Кухонная техника' недоступна"
+
+    def test_sport_button_is_available(self, home_page,page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._SPORT_BUTTON), "Кнопка 'Спорт и отдых' недоступна"
+
+    def test_furniture_button_is_available(self, home_page, page):
+        home_page.open_page()
+        assert home_page.is_element_visible(home_page._FURNITURE_BUTTON), "Кнопка 'Мебель' недоступна"
+
